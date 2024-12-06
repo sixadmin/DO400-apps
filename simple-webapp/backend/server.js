0f21d9dc6db12826c12bd555ea071970381c553a
Ricardo CRUZ <rchrdcruz@gmail.com>
2024-12-06 05:12:27 -0500

    Correction ordre  des variables

--- simple-webapp/backend/server.js
+++ simple-webapp/backend/server.js
@@ -10,17 +10,17 @@ const greet = require("./greet");
 //     res.end(greet(name));
 // });
 
-const port = 3000;
-server.listen(port, () => {
-    console.log(`Server listening on ${port}`);
-});
 
 const server = http.createServer((req, res) => {
     const { name } = url.parse(req.url, true).query;
     const greeting = greet(name);
-
+    
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/plain");
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.end(greeting);
+});
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server listening on ${port}`);
 });
